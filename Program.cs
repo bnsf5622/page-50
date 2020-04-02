490ea2a71bf90cf2beab518d3bbb51a5eeb02a83
Chuck <bnsf5622@gmail.com>
2020-04-02 15:05:23 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace page_50
+{
+    class Base
+    {
+        public int Add(int a1, int a2)
+        {
+            return a1 + a2;
+        }
+        public float Add(int a1, float a2)
+        {
+            return a1 + a2;
+        }
+    }
+    class Derived : Base
+    {
+        public int Add(int a1, int a2, int a3)
+        {
+            return a1 + a2 + a3;
+        }
+        public float Add(float a1, int a2)
+        {
+            return a1 + a2;
+        }
+    }
+
+    class MethodOverloadingUsage
+    {
+        static void Main()
+        {
+            Derived obj = new Derived();
+            Console.WriteLine(obj.Add(10, 20));
+            Console.WriteLine(obj.Add(10, 12.5f));
+            Console.WriteLine(obj.Add(10, 20, 30));
+            Console.WriteLine(obj.Add(13.5f, 10));
+            Console.Read();
+
+
+        }
+    }
+}
